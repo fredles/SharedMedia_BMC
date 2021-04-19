2a6682d1be056af56d8b63b1dcb1ae9e8978a333
Fredles <f_boisvert@outlook.com>
2021-04-19 04:52:25 -0400

    Files renamed + removed bloat

--- SharedMedia_BMC.lua
+++ SharedMedia_BMC.lua
@@ -1,22 +1,3 @@
 local LSM = LibStub("LibSharedMedia-3.0") 
 
--- ----- 
--- BACKGROUND 
--- ----- 
-
--- ----- 
---  BORDER 
--- ---- 
-
--- -----
---   FONT
--- -----
-
--- -----
---   SOUND
--- -----
-LSM:Register("sound", "Lean_SnakeFuck", [[Interface\Addons\SharedMedia_BMC\sound\Lean_SnakeFuck.ogg]])
-
--- -----
---   STATUSBAR
--- -----
+LSM:Register("sound", "Lean_SnakeFuck", [[Interface\Addons\SharedMedia_BMC\sound\Lean_Snake.ogg]])
