ba046c62da795b21236772a8b49d36b88b794d64
Fredles <f_boisvert@outlook.com>
2021-06-09 23:25:23 -0400

    Added Lean_Pig

--- SharedMedia_BMC.lua
+++ SharedMedia_BMC.lua
@@ -1,3 +1,4 @@
 local LSM = LibStub("LibSharedMedia-3.0") 
 
 LSM:Register("sound", "Lean_SnakeFuck", [[Interface\Addons\SharedMedia_BMC\sound\Lean_Snake.ogg]])
+LSM:Register("sound", "Lean_SnakeFuck", [[Interface\Addons\SharedMedia_BMC\sound\Lean_Pig.ogg]])
