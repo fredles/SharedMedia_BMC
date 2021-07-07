656ab79fd5529df825ae89745423afe2e84f3f95
Fredles <f_boisvert@outlook.com>
2021-07-07 14:53:07 -0400

    Added Johnny and Aki_Police soundbytes

--- SharedMedia_BMC.lua
+++ SharedMedia_BMC.lua
@@ -2,3 +2,8 @@ local LSM = LibStub("LibSharedMedia-3.0")
 
 LSM:Register("sound", "Lean_SnakeFuck", [[Interface\Addons\SharedMedia_BMC\sound\Lean_Snake.ogg]])
 LSM:Register("sound", "Lean_Pig", [[Interface\Addons\SharedMedia_BMC\sound\Lean_Pig.ogg]])
+LSM:Register("sound", "1_Johnny_WhatsUp", [[Interface\Addons\SharedMedia_BMC\sound\1_Johnny_WhatsUp.ogg]])
+LSM:Register("sound", "2_Johnny_Here", [[Interface\Addons\SharedMedia_BMC\sound\2_Johnny_Here.ogg]])
+LSM:Register("sound", "3_Johhny_HugeShoutout", [[Interface\Addons\SharedMedia_BMC\sound\3_Johhny_HugeShoutout.ogg]])
+LSM:Register("sound", "4_Johnny_FuckingBosses", [[Interface\Addons\SharedMedia_BMC\sound\4_Johnny_FuckingBosses.ogg]])
+LSM:Register("sound", "Aki_CallPolice", [[Interface\Addons\SharedMedia_BMC\sound\Aki_CallPolice.ogg]])
